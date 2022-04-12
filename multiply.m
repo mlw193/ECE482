deff51405519cbe52d139b1efd1334fbd745071d
mlw193 <103141949+mlw193@users.noreply.github.com>
2022-04-12 16:56:48 -0400

    Add files via upload

---
+++ multiply.m
@@ -0,0 +1,6 @@
+function mult = multiply(x,y)
+%UNTITLED2 Summary of this function goes here
+%   this function mult takes two inputs and multiplies them and gives the
+%   product
+       mult = x*y;
+end
